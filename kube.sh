0a7fe4f3620bd6a9cdce6f2e50fb6fb3a3e3b805
AhmedSRamadan <36161371+AhmedSRamadan@users.noreply.github.com>
2018-07-21 21:16:57 +0200

    Update kube.sh

--- kube.sh
+++ kube.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-kubectl apply -f drkiq-claim0-persistentvolumeclaim.yaml
-kubectl apply -fd rkiq-deployment.yaml
-kubectl apply -f drkiq-postgres-persistentvolumeclaim.yaml
-kubectl apply -f drkiq-redis-persistentvolumeclaim.yaml
-kubectl apply -f drkiq-service.yaml
-kubectl apply -f postgres-deployment.yaml
-kubectl apply -f postgres-service.yaml
-kubectl apply -f redis-deployment.yaml
-kubectl apply -f redis-service.yaml
-kubectl apply -f sidekiq-claim0-persistentvolumeclaim.yaml
-kubectl apply -f sidekiq-deployment.yaml
-kubectl apply -f sidekiq-service.yaml
+kubectl apply -f kubernetes/drkiq-claim0-persistentvolumeclaim.yaml
+kubectl apply -f kubernetes/drkiq-deployment.yaml
+kubectl apply -f kubernetes/drkiq-postgres-persistentvolumeclaim.yaml
+kubectl apply -f kubernetes/drkiq-redis-persistentvolumeclaim.yaml
+kubectl apply -f kubernetes/drkiq-service.yaml
+kubectl apply -f kubernetes/postgres-deployment.yaml
+kubectl apply -f kubernetes/postgres-service.yaml
+kubectl apply -f kubernetes/redis-deployment.yaml
+kubectl apply -f kubernetes/redis-service.yaml
+kubectl apply -f kubernetes/sidekiq-claim0-persistentvolumeclaim.yaml
+kubectl apply -f kubernetes/sidekiq-deployment.yaml
+kubectl apply -f kubernetes/sidekiq-service.yaml
